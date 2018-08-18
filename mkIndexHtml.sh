ff50e1ea3cdbd125f336996c342474ffb01df497
WpTestLabs <37196239+WpTestLabs@users.noreply.github.com>
2018-08-18 18:17:00 -0400

    Update mkIndexHtml.sh

--- mkIndexHtml.sh
+++ mkIndexHtml.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
-cat >$WwwDfltSwc/index.html <<EOF
+  echo "NginxConf/mkIndex.html $1 $2"
+cat >$1/index.html <<EOF
 <!DOCTYPE html><html><head>
-<title>index.html on default vhost</title>
+<title>index.html on $1 vhost</title>
 </head><body>
-index.html on default vhost
+index.html on $1 vhost<p>$2
 <p>Created: `date`
 </body></html>
 EOF
